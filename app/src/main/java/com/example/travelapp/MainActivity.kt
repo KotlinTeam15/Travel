6450f55d0588298b9a19f9c0f4f1575b62d36ab8
weize <weizechao27@gmail.com>
2022-10-31 14:45:39 -0400

    1st

--- app/src/main/java/com/example/travelapp/MainActivity.kt
+++ app/src/main/java/com/example/travelapp/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.travelapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val addcityBT = findViewById<Button>(R.id.addcityBT)
+
+        addcityBT.setOnClickListener {
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
